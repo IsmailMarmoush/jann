216ad1451a5c886bcd1a337323fa34844181e255
Ismail Marmoush <marmoushismail@gmail.com>
2012-06-12 18:23:37 +0200

    Signed-off-by: Ismail Marmoush <marmoushismail@gmail.com>

---
+++ src/main/java/com/marmoush/jann/utils/TrainUtils.java
@@ -0,0 +1,9 @@
+package com.marmoush.jann.utils;
+
+import org.jblas.DoubleMatrix;
+
+public class TrainUtils {
+    public static DoubleMatrix trainLinRegGd(DoubleMatrix error,DoubleMatrix input){
+	return error;
+    }
+}
