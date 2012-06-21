1e4ffb590143960bcedb44a5d7f4f7f60870d7da
Ismail Marmoush <marmoushismail@gmail.com>
2012-06-21 13:57:27 +0200

    Signed-off-by: Ismail Marmoush <marmoushismail@gmail.com>

--- src/main/java/com/marmoush/jann/model/regression/linear/LinearRegression.java
+++ src/main/java/com/marmoush/jann/model/regression/linear/LinearRegression.java
@@ -1,5 +1,7 @@
 package com.marmoush.jann.model.regression.linear;
 
+import java.util.List;
+
 import org.jblas.DoubleMatrix;
 
 import com.marmoush.jann.sv.SvLayer;
@@ -8,17 +10,31 @@ import com.marmoush.jann.utils.functors.ITransfere;
 import com.marmoush.jann.utils.functors.IWeight;
 
 public class LinearRegression extends SvLayer {
-    
+
     private static final long serialVersionUID = -8858564328695652268L;
 
     public LinearRegression(DoubleMatrix batchTrainingEx,
 	    DoubleMatrix batchTargets, boolean biased) {
-	super(batchTrainingEx.columns, 1,biased);
+	super(batchTrainingEx.columns, 1, biased);
 	setWeightFnctr(IWeight.BATCH_DOTPROD);
 	setTransfereFnctr(ITransfere.PURELIN);
 	setPerformancefnctr(IPerformance.MSE_LinRgr);
 	setLearnRate(0.01);
-	setFill(1, getWeight(), getBias());
+	setFill(1, getWeight());
+	if (biased)
+	    setFill(1, getBias());
+    }
+
+    public LinearRegression(List<DoubleMatrix> trainingEx,
+	    List<DoubleMatrix> targetList, boolean biased) {
+	super(trainingEx.get(0).rows, 1, biased);
+	setWeightFnctr(IWeight.DOTPROD);
+	setTransfereFnctr(ITransfere.PURELIN);
+	setPerformancefnctr(IPerformance.MSE_LinRgr);
+	setLearnRate(0.01);
+	setFill(1, getWeight());
+	if (biased)
+	    setFill(1, getBias());
     }
 
 }
