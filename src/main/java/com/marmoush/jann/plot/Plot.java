ecb89d4912663009c2507a93ad3ccc76a3ca571a
Ismail Marmoush <marmoushismail@gmail.com>
2012-06-23 09:57:10 +0200

    Signed-off-by: Ismail Marmoush <marmoushismail@gmail.com>

--- src/main/java/com/marmoush/jann/plot/Plot.java
+++ src/main/java/com/marmoush/jann/plot/Plot.java
@@ -1,5 +1,10 @@
 package com.marmoush.jann.plot;
 
 public class Plot {
-
+    public static void main(String[] args) {
+	PieChart demo = new PieChart("Comparison",
+		"Which operating system are you using?");
+	demo.pack();
+	demo.setVisible(true);
+    }
 }
